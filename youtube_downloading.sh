8df4f339ed1d439e161ee6dee5f605a44d192516
Praanshu Ubuntu22 Server <praanshugrover60@gmail.com>
2022-07-23 18:19:07 +0000

    Added code to create a txt file when script is running and delete after compeletion, this way cron can be set to run more frequently

--- youtube_downloading.sh
+++ youtube_downloading.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-source /home/praanshu/anaconda3/bin/activate yt_download &&
-cd /home/praanshu/yt_videos_download/ &&
-python src/playlist_download.py
-
-
+FILE=/home/praanshu/yt_downloader/script_running.txt
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+    echo "Script is running"
+else 
+    echo "$FILE does not exist."
+    echo " Starting Script "
+    source /home/praanshu/anaconda3/bin/activate youtube &&
+    cd /home/praanshu/yt_downloader/ && python src/playlist_download.py
+    rm /home/praanshu/yt_downloader/script_running.txt
+    echo "$File deleted"
+fi
 
